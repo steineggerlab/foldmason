6a27d9d0214bbaee69a9827e4796798098320c9b
Cameron Gilchrist <clmgilchrist@gmail.com>
2023-12-06 14:39:54 +0900

    limit threads

--- regression/regression/run_easymsa.sh
+++ regression/regression/run_easymsa.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
-"$FOLDMASON" easy-msa $(find "$DATADIR" -type f) "${RESULTS}/easymsa.fasta" "${RESULTS}/tmp" > "${RESULTS}/easymsa.log"
+"$FOLDMASON" easy-msa --threads 1 $(find "$DATADIR" -type f) "${RESULTS}/easymsa.fasta" "${RESULTS}/tmp" > "${RESULTS}/easymsa.log"
 TARGET="0.698404"
 awk -v target="$TARGET" \
     '/Average MSA LDDT/ {print ($4 == target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", $4; }' \
--- regression/regression/run_structuremsa.sh
+++ regression/regression/run_structuremsa.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
-"$FOLDMASON" createdb $(find "$DATADIR" -type f -name "d*") "${RESULTS}/structures"
-"$FOLDMASON" structuremsa "${RESULTS}/structures" "${RESULTS}/msa.fasta" > "${RESULTS}/structuremsa.log"
+"$FOLDMASON" createdb --threads 1 $(find "$DATADIR" -type f -name "d*") "${RESULTS}/structures"
+"$FOLDMASON" structuremsa --threads 1 "${RESULTS}/structures" "${RESULTS}/msa.fasta" > "${RESULTS}/structuremsa.log"
 if cmp -s "${RESULTS}/msa.fasta" "${DATADIR}/msa.fasta"; then
     echo "GOOD" > "${RESULTS}.report"
 else
