d0c4bf96d798cc2474a98f41d73b13967d9155a3
Milot Mirdita <milot@mirdita.de>
2025-10-08 16:35:46 +0900

    Update regression

--- regression/regression/run_easymsa.sh
+++ regression/regression/run_easymsa.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 "$FOLDMASON" easy-msa --report-mode 1 --threads 1 $(find "$DATADIR" -type f -name 'd*') "${RESULTS}/easymsa" "${RESULTS}/tmp" > "${RESULTS}/easymsa.log"
-TARGET="0.791274"
+TARGET="0.801621"
 awk -v target="$TARGET" \
     '/Average MSA LDDT/ {print ($4 == target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", $4; }' \
     "${RESULTS}/easymsa.log" > "${RESULTS}.report"
--- regression/regression/run_msa2lddt.sh
+++ regression/regression/run_msa2lddt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 "$FOLDMASON" createdb --threads 1 $(find "$DATADIR" -type f -name "d*") "${RESULTS}/structures"
 "$FOLDMASON" msa2lddt --threads 1 "${RESULTS}/structures" "${DATADIR}/msa.fasta" > "${RESULTS}/msa2lddt.log"
-TARGET="0.791274"
+TARGET="0.801621"
 awk -v target="$TARGET" \
     '/Average MSA LDDT/ {print ($4 == target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", $4; }' \
     "${RESULTS}/msa2lddt.log" > "${RESULTS}.report"
